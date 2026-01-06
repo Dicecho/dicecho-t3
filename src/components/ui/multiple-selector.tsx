cfdd1f4b01ab2c8fa0f0e4dfc2e722ef73d82d3f
painterpuppets <painterpuppets@gmail.com>
2026-01-06 18:33:44 +0900

    style: capitalize badge text in MultipleSelector component

--- src/components/ui/multiple-selector.tsx
+++ src/components/ui/multiple-selector.tsx
@@ -464,7 +464,7 @@ const MultipleSelector = React.forwardRef<MultipleSelectorRef, MultipleSelectorP
                 <Badge
                   key={option.value}
                   className={cn(
-                    'data-[disabled]:bg-muted-foreground data-[disabled]:text-muted data-[disabled]:hover:bg-muted-foreground',
+                    'data-[disabled]:bg-muted-foreground data-[disabled]:text-muted data-[disabled]:hover:bg-muted-foreground capitalize',
                     'data-[fixed]:bg-muted-foreground data-[fixed]:text-muted data-[fixed]:hover:bg-muted-foreground',
                     badgeClassName,
                   )}
