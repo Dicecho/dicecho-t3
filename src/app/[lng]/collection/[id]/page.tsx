38b98c0dd6c86f8a39319331fbb40c0b0997426e
painterpuppets <painterpuppets@gmail.com>
2026-01-15 16:11:30 +0900

    refactor: remove MobileFooter from CollectionDetailPage for cleaner layout

--- src/app/[lng]/collection/[id]/page.tsx
+++ src/app/[lng]/collection/[id]/page.tsx
@@ -24,7 +24,6 @@ export default async function CollectionDetailPage(props: {
       <div className="md:container md:py-6 max-md:pb-24">
         <CollectionDetail collection={collection} />
       </div>
-      <MobileFooter />
     </>
   );
 }
